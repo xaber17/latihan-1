1fc791780c1692ec855fbcd482ee0c1b92f8e882
xaber17 <55624860+xaber17@users.noreply.github.com>
2019-09-21 21:44:21 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,25 @@
+<?php
+$r = 14;
+$luaslingkaran = 22/7*$r^2;
+echo "Akulah Luas Lingkaran <br>";
+echo "Jika jari-jari ku adalah $r <br>";
+echo "Maka luasku adalah = $luaslingkaran";
+echo "<br> <br>";
+
+$a1 = 8;
+$t1 = 12;
+$luassegitiga = $a1*$t1/2;
+echo "Akulah Luas Segitiga <br>";
+echo "Jika tinggiku $t1 dan alasku $a1 <br>";
+echo "Maka luasku adalah = $luassegitiga";
+echo "<br> <br>";
+
+$a2 = 10;
+$t2 = 5;
+$luasgenjang = $a2*$t2;
+echo "Akulah Luas Jajar Genjang <br>";
+echo "Jika tinggiku $t2 dan alasku $a2 <br>";
+echo "Maka luasku adalah = $luasgenjang";
+echo "<br> <br>";
+
+?>
